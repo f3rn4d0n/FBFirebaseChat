c3805751751d22e478973c0037891b7adb64d361
Luis Fernando Bustos Ramírez <lufe.bura@gmail.com>
2019-03-17 12:02:14 -0600

    populate repo

---
+++ FBFirebaseChat/FBFirebaseChat.h
@@ -0,0 +1,19 @@
+//
+//  FBFirebaseChat.h
+//  FBFirebaseChat
+//
+//  Created by Luis Fernando Bustos Ramírez on 3/17/19.
+//  Copyright © 2019 Gastando Tenis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBFirebaseChat.
+FOUNDATION_EXPORT double FBFirebaseChatVersionNumber;
+
+//! Project version string for FBFirebaseChat.
+FOUNDATION_EXPORT const unsigned char FBFirebaseChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBFirebaseChat/PublicHeader.h>
+
+
